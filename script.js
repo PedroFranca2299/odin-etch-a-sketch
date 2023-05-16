8643cac41ad27e8e0da11f20179f88e95910c4df
Pedro Henrique <pedronutti@gmail.com>
2023-05-16 11:45:32 -0300

    Add drawing feature

--- script.js
+++ script.js
@@ -2,7 +2,18 @@ let quant;
 const button = document.getElementById('sqr-num');
 button.addEventListener('click', function () {
     const userInput = prompt('Type how many squares per side you wish!')
-    quant = userInput;
+    quant = parseInt(userInput);
+
+    while (isNaN(quant)) {
+        const userInput = prompt('Type a number please!');
+        quant = parseInt(userInput);
+    }
+
+    while (quant > 100) {
+        const userInput = prompt('Input exceeds the max value of 100!');
+        quant = parseInt(userInput);
+    }
+
     console.log(quant);
     const container = document.querySelector('.sqr-container');
     
@@ -19,6 +30,9 @@ button.addEventListener('click', function () {
         for (let i = 1; i <= quant; i++) {
             const sqr = document.createElement('div');
             sqr.classList.add('block')
+            sqr.addEventListener('mouseover', function() {
+                sqr.classList.add('new-color');
+            })
             row.appendChild(sqr);
             // sqr.style.width = "50px";
             // sqr.style.height = "50px";
@@ -26,7 +40,9 @@ button.addEventListener('click', function () {
         }
         container.appendChild(row);
     }  
+    
 });
+    
 
  
 
